6189d8b1baab2495cf612e7076486e5570592748
wangyawei <wangyawei@wondersgroup.com>
2024-12-18 09:42:56 +0800

    pods

---
+++ Pod1ViewController.h
@@ -0,0 +1,16 @@
+//
+//  Pod1ViewController.h
+//  Pod
+//
+//  Created by wonders on 2024/12/18.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Pod1ViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Pod1ViewController.m
@@ -0,0 +1,31 @@
+//
+//  Pod1ViewController.m
+//  Pod
+//
+//  Created by wonders on 2024/12/18.
+//
+
+#import "Pod1ViewController.h"
+
+@interface Pod1ViewController ()
+
+@end
+
+@implementation Pod1ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
